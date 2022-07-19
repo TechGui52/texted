2814d18e92254177ba9682eeaed9f96502aa8e8b
TechGui52 <TechGui52@gmail.com>
2022-07-19 14:03:50 -0400

    functioning buttions updated readMe still needs design

--- texted.js
+++ texted.js
@@ -2,23 +2,31 @@ function bold() {
     document.getElementById("textarea1").style.fontWeight = "bold";
 }
 function italic() {
-    document.getElementById("textarea1").style.fontStyle = "italic"
+    document.getElementById("textarea1").style.fontStyle = "italic";
 }
 function left() {
-    document.getElementById("textarea1").style.textAlign ="left"
+    document.getElementById("textarea1").style.textAlign ="left";
 }
 function center() {
-    document.getElementById("textarea1").style.textAlign ="center"
+    document.getElementById("textarea1").style.textAlign ="center";
 }
 function right() {
-    document.getElementById("textarea1").style.textAlign ="right"
+    document.getElementById("textarea1").style.textAlign ="right";
 }
 function lowercase() {
-    document.getElementById("textarea1").style.textTransform ="lowercase"
+    document.getElementById("textarea1").style.textTransform ="lowercase";
 }
 function uppercase() {
-    document.getElementById("textarea1").style.textTransform ="uppercase"
+    document.getElementById("textarea1").style.textTransform ="uppercase";
 }
-function clear() {
-    document.getElementById("textarea1").value =""
+function capitalize() {
+    document.getElementById("textarea1").style.textTransform="capitalize"
 }
+function clean() {
+    document.getElementById("textarea1").value="";
+    document.getElementById("textarea1").style.fontWeight="normal";
+    document.getElementById("textarea1").style.fontStyle="normal";
+    document.getElementById("textarea1").style.textAlign="left";
+    document.getElementById("textarea1").style.textTransform="none";
+}
+
